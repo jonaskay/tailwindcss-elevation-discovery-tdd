3efa25ea2a067eafed7d2cfc232865d9bbdff705
Joonas Kykkänen <jonaskay@iki.fi>
2022-10-20 10:24:43 +0300

    Don't compile test files

--- dist/src/hello-world.js
+++ dist/src/hello-world.js
@@ -1,4 +1,4 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-function default_1() { }
-exports.default = default_1;
+exports.helloWorld = void 0;
+exports.helloWorld = "Hello, World!";
